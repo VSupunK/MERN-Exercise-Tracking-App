526340a72cde772c21effed89c59942b870d19c4
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-08-29 21:04:30 +0530

    updated App.js

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -43,14 +43,14 @@ router.route('/update/:id').post((req, res) => {
     .then(exercise => {
         exercise.username = req.body.username;
         exercise.description = req.body.description;
-        exercise.duration = req.body.duration;
-        exercise.date = req.body.date;
+        exercise.duration = Number(req.body.duration);
+        exercise.date = Date.parse(req.body.date);
     
         exercise.save()
             .then(() => json('Exercise Updated!'))
             .catch(err => res.status(400).json('Error: ' + err));
     })
     .catch(err => res.status(400).json('Error: '+err));
-})
+});
 
 module.exports = router;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'boostrap/dist/css/boostrap.min.css'
+
+import Navbar from "./component/navbar.component";
+import ExerciseList from "./component/exercise-list.component";
+import EditExercise from "./component/edit-exercise.component";
+import CreateExercise from "./component/create-exercise.component";
+import CreateUser from "./component/cretae-user.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <br/>
+
+      <Route path='/' exact component={ExerciseList} />
+      <Route path='/edit/:id' component={EditExercise} />
+      <Route path='/create' component={CreateExercise} />
+      <Route path='/user' component={CreateUser} />
+    </Router>
   );
 }
 
